b35f35e021ec943ca019830d38d73a00067b5e56
Kieran Walsh <kieran@memorylane.digital>
2026-02-04 18:59:30 +1000

    Fix WPF usings for build on macOS

--- OutlookExportTool/src/OutlookExportTool.WPF/OutlookExportWorker.cs
+++ OutlookExportTool/src/OutlookExportTool.WPF/OutlookExportWorker.cs
@@ -1,4 +1,9 @@
+using System;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Office.Interop.Outlook;
 using OutlookExportTool.Core.Models;
 using OutlookExportTool.Core.Services;
